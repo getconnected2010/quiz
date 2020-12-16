05e33920c99800bd8a8e9e774f4bcbd458e12f20
getconnected2010 <65646133+getconnected2010@users.noreply.github.com>
2020-12-15 21:15:14 -0500

    added a home page
    A home page with a background image and descripition of the app.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import './home.css';
 
 const Home = () => {
     return (
-        <div>
-            This is home page
+        <div className='HomePage'>
+            <h1>Welcome to your quiz app</h1>
+            <p>
+                Have you wanted to quiz someone else based on your knowledge? 
+                Well, this is exactly what this app will do.
+                Click on the 'Add to Quiz' tab and enter a question with four options for an answer.
+                Hint...one of the four questions should be the correct answer.
+                Enter as many questions and answers you like.
+                Then select 'Take Quiz' tab where you will find a list of all the questions and answer options.
+                Select an answer for each question and click 'Submit'. Your answers will be verified and a score will
+                display on the screen.
+                Happy playing...
+            </p>
         </div>
     )
 }
