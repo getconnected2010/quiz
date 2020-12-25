13aa349c0ff431bb2cbef9f8d6f9e78b61a31a82
getconnected2010 <65646133+getconnected2010@users.noreply.github.com>
2020-12-25 01:28:24 -0500

    fixed a redux error
    fixed a user status update in redux

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import{Link, useHistory} from 'react-router-dom'
-import{useSelector} from 'react-redux'
-import{Form} from 'formik'
+import{useSelector, useDispatch} from 'react-redux'
 import './nav.css';
 import {signout} from '../services/api/userApi'
 import {signOutAction} from '../actions/userActions'
 
 const Nav = () => {
     const history= useHistory()
+    const dispatch=useDispatch()
     const user = useSelector(state=>state.user)
+    console.log(user)
     const user_id= user.user_id
     const admin= user.admin
-    console.log(user_id)
     const logout=async()=>{
         await signout()
-        signOutAction()
+        dispatch(signOutAction())
         history.push('/')
     }
     return (
@@ -22,16 +22,14 @@ const Nav = () => {
             <Link to='/'>Home</Link>
             <Link to='/add'>Add to Quiz</Link>
             <Link to='/list'>Take Quiz</Link>
-            <div>
+            <>
             {
                 user_id?
-                    <form>
-                        <button type='submit' onClick={logout}>Logout</button>
-                    </form>
+                    <button style={{borderRadius:'10px', padding:'5px 15px', background:'green', border:'2px solid white', color:'white', fontWeight:'bolder'}} onClick={logout} type='submit' >Logout</button>
                     :
-                    <button onClick={()=>history.push('/signin')}>Login</button>
+                    <button style={{borderRadius:'10px', padding:'5px 15px', background:'green', border:'2px solid white', color:'white', fontWeight:'bolder'}} onClick={()=>history.push('/signin')}>Login</button>
             }
-            </div>
+            </>
         </div>
     )
 }
