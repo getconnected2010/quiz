ae5c0ade25f3686e744c8020bffd8ec784cdbd41
getconnected2010 <65646133+getconnected2010@users.noreply.github.com>
2021-01-19 21:07:14 -0500

    removed bcrypt

--- server/index.js
+++ server/index.js
@@ -10,10 +10,10 @@ const cookieParser= require('cookie-parser')
 const whiteList=['http://localhost:3000', 'http://localhost:8000']
 const corsOptions={
     origin: (origin, callback)=>{
-        if(whiteList.indexOf(origin)!== -1){
+        if(!origin || whiteList.indexOf(origin)!== -1){
             callback(null, true)
         } else{
-            callback(new Error('Not allowed by CORS'))
+            callback(new Error(`Not allowed by CORS ${origin}`))
         }
     }
 }
